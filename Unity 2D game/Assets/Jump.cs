006729cb0cefb072fae732f13ec51a9b7fb198f4
Geovanni Orozco <gxorozco02@gmail.com>
2016-09-21 16:48:59 -0700

    The actual project

---
+++ Unity 2D game/Assets/Jump.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    Vector2 jumpman = new Vector2(0, 500);
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Jump"))
+        {
+            rb.AddForce(jumpman);
+        }
+    }
+}
