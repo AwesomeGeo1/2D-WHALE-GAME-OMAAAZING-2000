f5187ab821665dfc0dd6856be7608eb7014ddb08
Wesley <wes@itsa.academy>
2016-09-21 17:54:39 -0700

    movement
    The whale moves forwards now YAY!!!

---
+++ Unity 2D game/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public Rigidbody2D ridgidBody;
+
+    Vector2 moveForwards = new Vector2(100, 0);
+
+	// Use this for initialization
+	void Start () {
+        ridgidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            ridgidBody.AddForce(moveForwards);
+        }
+	}
+}
