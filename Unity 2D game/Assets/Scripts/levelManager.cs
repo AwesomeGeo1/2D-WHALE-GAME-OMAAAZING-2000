516a9fc985433d3c0260a0390de505c66cd48cf0
Wesley <wes@itsa.academy>
2016-09-23 14:09:42 -0700

    flag is semi operational
    i got it so the flag registers when the whale hits it. Next I need it to
    load a new level

---
+++ Unity 2D game/Assets/Scripts/levelManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelManager : MonoBehaviour {
+    bool control = false;
+	// Use this for initialization
+	void Start () {
+      
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.Find("flagPole").GetComponent<EndGame>().levelComplete)
+        {
+            
+
+            if( control == false)
+            {
+                control = true;
+                Debug.Log(control);
+               
+            }
+        }
+       
+
+    }
+}
