6623f405895100dbc72572cbcebf17d5b05be2f7
Geovanni Orozco <gxorozco02@gmail.com>
2016-09-26 16:34:06 -0700

    Added the evil laser cat

---
+++ Unity 2D game/Assets/Scripts/Laserkittenenimiescript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Laserkittenenimiescript : MonoBehaviour {
+    public float movementSpeed = 10;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex;
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+
+
+    }
+    // Update is called once per frame
+    void Update () {
+        move();
+    
+    }
+    void move()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime);
+
+        transform.Translate(Vector3.right * Time.deltaTime);
+    }
+}
